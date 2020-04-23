2c1130874bca860a53f27790bb4a8910e9e220e6
menjiasong00 <526762400@qq.com>
2020-04-23 17:21:43 +0800

    text

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
 
 #打镜像
-docker build -f runing/app.df -t testapp:latest .
+#docker build -f runing/app.df -t testapp:latest .
 
 #把镜像打tag变成和仓库一致
-docker tag  testapp:lastest 127.0.0.1:5000/myapp:lastest
+#docker tag  testapp:lastest 127.0.0.1:5000/myapp:lastest
 
 
 #推到镜像上去
-docker push 127.0.0.1:5000/myapp:lastest
+#docker push 127.0.0.1:5000/myapp:lastest
 
 #起compose
-docker-compose -f runing/docker.yaml pull
-docker-compose -f runing/docker.yaml up
+#docker-compose -f runing/docker.yaml pull
+#docker-compose -f runing/docker.yaml up
  
--- pb/test.pb.gw.go
+++ pb/test.pb.gw.go
@@ -64,40 +64,6 @@ func local_request_Test_GetTestMsg_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
-func request_Test_MakeCoding_0(ctx context.Context, marshaler runtime.Marshaler, client TestClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq MakeCodingRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.MakeCoding(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_Test_MakeCoding_0(ctx context.Context, marshaler runtime.Marshaler, server TestServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq MakeCodingRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.MakeCoding(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
 // RegisterTestHandlerServer registers the http handlers for service Test to "mux".
 // UnaryRPC     :call TestServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -120,26 +86,6 @@ func RegisterTestHandlerServer(ctx context.Context, mux *runtime.ServeMux, serve
 		}
 
 		forward_Test_GetTestMsg_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("POST", pattern_Test_MakeCoding_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_Test_MakeCoding_0(rctx, inboundMarshaler, server, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Test_MakeCoding_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -204,37 +150,13 @@ func RegisterTestHandlerClient(ctx context.Context, mux *runtime.ServeMux, clien
 
 	})
 
-	mux.Handle("POST", pattern_Test_MakeCoding_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_Test_MakeCoding_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Test_MakeCoding_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	return nil
 }
 
 var (
 	pattern_Test_GetTestMsg_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "test"}, "", runtime.AssumeColonVerbOpt(true)))
-
-	pattern_Test_MakeCoding_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"base", "coding"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
 	forward_Test_GetTestMsg_0 = runtime.ForwardResponseMessage
-
-	forward_Test_MakeCoding_0 = runtime.ForwardResponseMessage
 )
