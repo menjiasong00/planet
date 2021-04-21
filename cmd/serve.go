426f09e454dbbb9da61ad6058dec2cb462ebb038
runoob <test@runoob.com>
2021-04-21 17:25:33 +0800

    拆分微服务

--- cmd/serve.go
+++ cmd/serve.go
@@ -9,38 +9,46 @@ import (
 )
 
 func init() {
+	//服务
 	RootCmd.AddCommand(serveCmd)
+	//网关
+	RootCmd.AddCommand(gatewayCmd)
+	//单体服务
 	RootCmd.AddCommand(DevCmd)
 }
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "Launches the example webserver on  "+demoAddr,
+	Short: "Launches the serve ",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args)>0 {
 			servFlag := args[0]
 			grpcPort := env.Config.GetString("Server."+servFlag+".GrpcPort")
-			httpPort := env.Config.GetString("Server."+servFlag+".HttpPort")
-			if servFlag == "GateWay" {
-				//网关
-				gcore.RunServeHTTP(server.ConfigServer(servFlag),httpPort)
-			}else{
-				//各个rpc服务
-				gcore.RunServeGRPC(server.ConfigServer(servFlag),grpcPort)
-				/*var rbmqConfig grbmq.ConnConfig
-				env.ScanConfig("Rbmq",&rbmqConfig)
-				grbmq.New(rbmqConfig).RunConsumers(mq.ConfigConsumer)*/
-			}
+			//各个rpc服务
+			gcore.RunServeGRPC(server.ConfigServer(servFlag),grpcPort)
+			/*var rbmqConfig grbmq.ConnConfig
+			env.ScanConfig("Rbmq",&rbmqConfig)
+			grbmq.New(rbmqConfig).RunConsumers(mq.ConfigConsumer)*/
 		}
 	},
 }
 
-
 // serveCmd represents the serve command
+var gatewayCmd = &cobra.Command{
+	Use:   "gateway",
+	Short: "Launches the gateway  ",
+	Run: func(cmd *cobra.Command, args []string) {
+		servFlag := "GateWay"
+		httpPort := env.Config.GetString("Server."+servFlag+".HttpPort")
+		gcore.RunServeHTTP(server.ConfigServer(servFlag),httpPort)
+	},
+}
+
+// serveCmd represents the dev command
 var DevCmd = &cobra.Command{
 	Use:   "dev",
-	Short: "Launches the example webserver on  "+demoAddr,
+	Short: "Launches the example webserver  ",
 	Run: func(cmd *cobra.Command, args []string) {
 		//开发环境直接起
 		servFlag := "Dev"
@@ -55,6 +63,5 @@ var DevCmd = &cobra.Command{
 		gcore.RunServeHTTP(server.ConfigServer(servFlag),httpPort)
 		//gcore.MakeInsecure(insecure.Key,insecure.Cert)
 		//serve()
-
 	},
 }
